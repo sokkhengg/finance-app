2b9a98c324883f549f69a6caead2dc7db33a2bd3
sokkhengg <sokhengteangg@gmail.com>
2025-04-23 06:52:51 -0700

    implement dark mode, fetching and display data

---
+++ app/dashboard/components/trend-fallback.js
@@ -0,0 +1,18 @@
+import Skeleton from "@/components/skeleton";
+
+export default function TrendFallBack() {
+  return (
+    <div className="space-y-5 w-3/5 lg:w-5/6">
+    <div>
+        <Skeleton/>
+    </div>
+    <div className="mb-2">
+    <Skeleton/>
+    </div>
+    <div className="flex space-x-2">
+    <Skeleton/>
+      
+    </div>
+  </div>
+  )
+}
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,27 @@
 import { Suspense } from "react";
 import TransactionList from "./components/transaction-list";
 import TransactionListFallBack from "@/components/transaction-list-fallback";
+import Trend from "./components/trend";
+import TrendFallBack from "./components/trend-fallback";
 
 
 export default function Page() {
   return (
     <>
+    <section className="mb-8 gird grid-cols-2 lg:grid-cols-4">
+      <Suspense fallback={<TrendFallBack/>}>
+        <Trend type="Income"/>
+      </Suspense>
+      <Suspense fallback={<TrendFallBack/>}>
+        <Trend type="Expense"/>
+      </Suspense>
+      <Suspense fallback={<TrendFallBack/>}>
+        <Trend type='Saving'/>
+      </Suspense>
+      <Suspense fallback={<TrendFallBack/>}>
+        <Trend type="Investment"/>
+      </Suspense>
+    </section>
     <Suspense fallback={<TransactionListFallBack/>}>
       <TransactionList/>
 
