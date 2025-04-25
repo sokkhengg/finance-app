41bc1d77dd0fe06b645dbb3bd99ebde4262d3adf
sokkhengg <sokhengteangg@gmail.com>
2025-04-25 13:03:01 -0700

    Customize SQL function to filter  transaction based on the range of the days

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,44 +1,43 @@
-import { Suspense } from "react";
-import TransactionList from "./components/transaction-list";
-import TransactionListFallBack from "@/components/transaction-list-fallback";
-import Trend from "./components/trend";
-import TrendFallBack from "./components/trend-fallback";
-import Link from "next/link";
-import { PlusCircle } from "lucide-react";
-import {sizes, variants} from "@/lib/variants"
-import { types } from "@/lib/consts";
+import { Suspense } from "react"
+import TransactionList from "./components/transaction-list"
+import TransactionListFallback from "./components/transaction-list-fallback"
+import Trend from "./components/trend"
+import TrendFallback from "./components/trend-fallback"
+import Link from 'next/link'
+import { PlusCircle } from 'lucide-react'
+import { sizes, variants } from "@/lib/variants"
 import { ErrorBoundary } from "react-error-boundary";
-import Range from "./components/range";
+import { types } from "@/lib/consts"
+import Range from "./components/range"
 
-
-export default async function Page() {
-  
-  return (
-    <>
+export default async function Page({ searchParams }) {
+  const range = searchParams?.range ?? 'last30days'
+  return (<>
     <section className="mb-8 flex justify-between items-center">
       <h1 className="text-4xl font-semibold">Summary</h1>
       <aside>
-        <Range/>
+        <Range />
       </aside>
     </section>
-    <section className="mb-8 gird grid-cols-2 lg:grid-cols-4">
+
+    <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
       {types.map(type => <ErrorBoundary key={type} fallback={<div className="text-red-500">Cannot fetch {type} trend data</div>}>
-        <Suspense fallback={<TrendFallBack/>}>
-          <Trend type={type}/>
+        <Suspense fallback={<TrendFallback />}>
+          <Trend type={type} range={range} />
         </Suspense>
       </ErrorBoundary>)}
     </section>
+
     <section className="flex justify-between items-center mb-8">
-      <h2 className="text-2xl">Trasactions</h2>
-      <Link href="dashboard/transaction/add" className={`flex items-center space-x-1 ${variants["outline"]}`}>
-      <PlusCircle/>
-      <div>Add</div>
+      <h2 className="text-2xl">Transactions</h2>
+      <Link href="/dashboard/transaction/add" className={`flex items-center space-x-1 ${variants['outline']} ${sizes['sm']}`}>
+        <PlusCircle className="w-4 h-4" />
+        <div>Add</div>
       </Link>
     </section>
-    <Suspense fallback={<TransactionListFallBack/>}>
-      <TransactionList/>
 
+    <Suspense fallback={<TransactionListFallback />}>
+      <TransactionList />
     </Suspense>
-    </>
-  )
+  </>)
 }
--- lib/supabase/calculate-total.sql
+++ lib/supabase/calculate-total.sql
@@ -1,15 +1,38 @@
--- Create or replace the existing function, named `calculate_total`
--- Note: if your function has a different argument set, it won't replace the original function
 create or replace function calculate_total (
-  -- The arguments (this one is optional, but arguments can be mandatory, just skip `default null`)
-	type_arg varchar default null
-) returns numeric as $$ -- Returns number
-begin
-	return (
-		select SUM(amount) -- Select the sum of all values from the amount column
-		from transactions  -- of transactions table
-		where              -- where...
-			(type = type_arg or type_arg is null) -- The type is type_arg, if type_arg was specified
-	);
+  range_arg varchar default 'last30days',
+  type_arg varchar default null
+  ) returns table(current_amount numeric, previous_amount numeric) as $$ 
+declare
+currentStart timestamp;
+currentEnd timestamp;
+previousStart timestamp;
+previousEnd timestamp;
+
+begin 
+currentEnd := now();
+currentStart := case
+when range_arg = 'last24hours' then currentEnd - interval '24 hour'
+when range_arg = 'last7days' then currentEnd - interval '7 days'
+when range_arg = 'last30days' then currentEnd - interval '30 days'
+when range_arg = 'last12months' then currentEnd - interval '12 months'
+else currentEnd - interval '30 days'
 end;
-$$ language plpgsql -- The language of this func is plpgsql (other languages can be used!)
+previousEnd := currentStart - interval '1 second';
+previousStart := currentStart - (currentEnd - currentStart);
+current_amount := (
+  select COALESCE(SUM(amount), 0)
+  from transactions
+  where
+  (type = type_arg or type_arg is null)
+  and (created_at between currentStart and currentEnd)
+  );
+  previous_amount := (
+    select COALESCE(SUM(amount), 0)
+    from transactions
+    where
+    (type = type_arg or type_arg is null)
+    and (created_at between previousStart and previousEnd)
+  );
+  return next;  
+end;
+$$ language plpgsql
