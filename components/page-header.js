cb5c75554cd51f2a37df016e37c1458af456665f
sokkhengg <sokhengteangg@gmail.com>
2025-05-02 16:16:10 -0700

    Implemented uploading and managing files

--- components/page-header.js
+++ components/page-header.js
@@ -2,14 +2,16 @@ import Link from 'next/link'
 import DarkModeToggle from './dark-mode-toggle'
 import useServerDarkMode from '@/hooks/use-server-dark-mode'
 import { createClient } from '@/lib/supabase/server'
-import { CircleUser, KeyRound } from 'lucide-react'
+import { KeyRound } from 'lucide-react'
 import { sizes, variants } from '@/lib/variants'
 import SignOutButton from './sign-out-button'
+import Avatar from './avatar'
 
 export default async function PageHeader({ className }) {
   const theme = useServerDarkMode()
   const supabase = createClient()
   const { data: { user }, error } = await supabase.auth.getUser()
+  console.log(user)
   return (
     <header className={`flex justify-between items-center ${className}`}>
       <Link href="/dashboard" className="text-xl hover:underline underline-offset-8 decoration-2">Finance App</Link>
@@ -17,7 +19,7 @@ export default async function PageHeader({ className }) {
       <div className="flex items-center">
         <DarkModeToggle defaultMode={theme} />
         {user && <Link href="/dashboard/settings" className={`flex items-center space-x-1 ${variants['ghost']} ${sizes['sm']}`}>
-          <CircleUser className="w-6 h-6" />
+          <Avatar />
           <span>{user?.email}</span>
         </Link>}
         {user && <SignOutButton />}
